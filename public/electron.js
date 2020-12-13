a449d891198fb5d05f78f1d2ab6acf08c05a71a3
Akash <akash.joshi@viit.ac.in>
2020-12-13 15:36:03 +0530

    Updoot

--- public/electron.js
+++ public/electron.js
@@ -1,15 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
-const pdfWindow = require('electron-pdf-window');
-const path = require('path');
-const argv = require('yargs').parse(process.argv.slice(1));
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
+const argv = require("yargs").parse(process.argv.slice(1));
 
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const os = require('os');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const os = require("os");
 
-const { setMainMenu } = require('./menu');
+const { setMainMenu } = require("./menu");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,8 +18,11 @@ let mainWindow;
 const flashPath = path.join(os.homedir(), ".pennywise-flash");
 if (flashPath && fs.existsSync(flashPath)) {
   try {
-    app.commandLine.appendSwitch('ppapi-flash-path', fs.realpathSync(flashPath));
-    console.log("Attempting to load flash at " + flashPath)
+    app.commandLine.appendSwitch(
+      "ppapi-flash-path",
+      fs.realpathSync(flashPath)
+    );
+    console.log("Attempting to load flash at " + flashPath);
   } catch (e) {
     console.log("Error finding flash at " + flashPath + ": " + e.message);
   }
@@ -28,31 +30,30 @@ if (flashPath && fs.existsSync(flashPath)) {
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    title: 'Pennywise',
+    opacity: 0.9,
+    title: "Notescratch",
     width: 700,
     height: 600,
     autoHideMenuBar: true,
-    backgroundColor: '#16171a',
+    backgroundColor: "#16171a",
     show: false,
     frame: argv.frameless ? false : true,
     webPreferences: {
       plugins: true,
       nodeIntegration: true,
-      webviewTag: true
+      webviewTag: true,
     },
   });
-
-  pdfWindow.addSupport(mainWindow);
 
   const isDev = !!process.env.APP_URL;
   if (process.env.APP_URL) {
     mainWindow.loadURL(process.env.APP_URL);
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+    mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
   }
 
   // Show the window once the content has been loaded
-  mainWindow.on('ready-to-show', () => {
+  mainWindow.on("ready-to-show", () => {
     // Hide the dock icon before showing and
     // show it once the app has been displayed
     // @link https://github.com/electron/electron/issues/10078
@@ -69,7 +70,7 @@ function createWindow() {
     bindIpc();
   });
 
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 
@@ -85,24 +86,24 @@ function createWindow() {
 // Binds the methods for renderer/electron communication
 function bindIpc() {
   // Binds the opacity getter functionality
-  ipcMain.on('opacity.get', (event) => {
+  ipcMain.on("opacity.get", (event) => {
     // Multiplying by 100 – browser range is 0 to 100
     event.returnValue = mainWindow.getOpacity() * 100;
   });
 
-  ipcMain.on('opacity.set', (event, opacity) => {
+  ipcMain.on("opacity.set", (event, opacity) => {
     // Divide by 100 – window range is 0.1 to 1.0
     mainWindow.setOpacity(opacity / 100);
   });
 
-  ipcMain.on('loadPDF', (event, file) =>{
-    mainWindow.loadURL(file)
-  })
+  ipcMain.on("loadPDF", (event, file) => {
+    mainWindow.loadURL(file);
+  });
 }
 
 // Makes the app start receiving the mouse interactions again
 function disableDetachedMode() {
-  app.dock && app.dock.setBadge('');
+  app.dock && app.dock.setBadge("");
   mainWindow && mainWindow.setIgnoreMouseEvents(false);
 }
 
@@ -121,38 +122,38 @@ function checkAndDownloadUpdate() {
 function listenUrlLoader() {
   const server = http.createServer((request, response) => {
     let target_url = url.parse(request.url, true).query.url;
-    target_url = Array.isArray(target_url) ? target_url.pop : '';
+    target_url = Array.isArray(target_url) ? target_url.pop : "";
 
     if (target_url) {
-      mainWindow.webContents.send('url.requested', target_url);
+      mainWindow.webContents.send("url.requested", target_url);
     }
 
     response.writeHeader(200);
     response.end();
   });
 
-  server.listen(6280, '0.0.0.0');
+  server.listen(6280, "0.0.0.0");
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function () {
+app.on("ready", function () {
   createWindow();
   checkAndDownloadUpdate();
   listenUrlLoader();
 });
 
 // Make the window start receiving mouse events on focus/activate
-app.on('browser-window-focus', disableDetachedMode);
-app.on('activate', disableDetachedMode);
+app.on("browser-window-focus", disableDetachedMode);
+app.on("activate", disableDetachedMode);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   app.quit();
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
--- src/browser.js
+++ src/browser.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import EmptyPage from './components/empty-page';
-import WebPage from './components/web-page';
-import { prepareUrl } from './utils/helpers';
+import EmptyPage from "./components/empty-page";
+import WebPage from "./components/web-page";
+import { prepareUrl } from "./utils/helpers";
 
-const { ipcRenderer, remote } = window.require('electron');
-const yargs = window.require('yargs').parse(remote.process.argv.slice(1));
+const { ipcRenderer } = window.require("electron");
 
 class Browser extends React.Component {
   state = {
-    url: yargs.url ? prepareUrl(yargs.url) : '',
-    embedVideosEnabled: true
+    url: "https://aknote.vercel.app",
+    // url: yargs.url ? prepareUrl(yargs.url) : "",
+    embedVideosEnabled: true,
   };
 
   onUrl = (url) => {
@@ -24,26 +24,22 @@ class Browser extends React.Component {
   };
 
   onUrlRequested = (event, url) => {
-    this.onUrl(url)
+    this.onUrl(url);
   };
 
   componentDidMount() {
-    ipcRenderer.on('embedVideos.set', this.onembedVideosSet);
-    ipcRenderer.on('url.requested', this.onUrlRequested);
-  }
-
-  componentWillUnmount() {
-    ipcRenderer.removeEventListener('embedVideos.set', this.onembedVideosSet);
+    ipcRenderer.on("embedVideos.set", this.onembedVideosSet);
+    ipcRenderer.on("url.requested", this.onUrlRequested);
   }
 
   render() {
     return (
-      <div className='browser-wrap'>
-        {
-          this.state.url
-            ? <WebPage url={ this.state.url } onUrl={ this.onUrl }/>
-            : <EmptyPage onUrl={ this.onUrl }/>
-        }
+      <div className="browser-wrap">
+        {this.state.url ? (
+          <WebPage url={this.state.url} onUrl={this.onUrl} />
+        ) : (
+          <EmptyPage onUrl={this.onUrl} />
+        )}
       </div>
     );
   }
